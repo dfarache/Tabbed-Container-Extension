693ba76aecba05cdd0d85cb4a181c53e37040934
bcf <bcf@qlik.com>
2017-05-30 15:19:09 +0200

    Removing unused depency injection and refatoring service

--- services/tabService.js
+++ services/tabService.js
@@ -1,34 +1,32 @@
 define(["qvangular"], function(qva) {
     "use strict";
 
-    qva.service("tabService", ["$q",
-        function(q) {
+    qva.service("tabService", function() {
 
-            return {
+        return {
 
-                getTabInfo: function($scope) {
-                    var tabItems = [];
-                    var maxTabItems = 5;
-                    var objectid, tab;
+            getTabInfo: function($scope) {
+                var tabItems = [];
+                var maxTabItems = 5;
+                var objectid, tab;
 
-                    for(var i=1; i<=maxTabItems; i++) {
-                        tab = $scope.layout.props['tab' + i];
+                for(var i=1; i<=maxTabItems; i++) {
+                    tab = $scope.layout.props['tab' + i];
 
-                        if(tab.title.length > 0 && (tab.masterItem.length > 0 || tab.objectid.length > 0)) {
-                            objectid = (tab.objectid.length > 0) ? tab.objectid : tab.masterItem;
+                    if(tab.title.length > 0 && (tab.masterItem.length > 0 || tab.objectid.length > 0)) {
+                        objectid = (tab.objectid.length > 0) ? tab.objectid : tab.masterItem;
 
-                            tabItems.push({
-                                id: i + objectid,
-                                objectid: objectid,
-                                title: tab.title,
-                                index: i
-                            })
-                        }
+                        tabItems.push({
+                            id: i + objectid,
+                            objectid: objectid,
+                            title: tab.title,
+                            index: i
+                        })
                     }
-
-                    return tabItems;
                 }
+
+                return tabItems;
             }
         }
-    ])
+    })
 });
