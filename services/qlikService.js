d97b8c22b25d0a748cb690f21acd91652987a045
bcf <bcf@qlik.com>
2017-06-14 10:11:50 +0200

    Appending the headers of the object to the top of the data model

--- services/qlikService.js
+++ services/qlikService.js
@@ -21,28 +21,28 @@ define(['qlik', 'qvangular', 'angular'], function(qlik, qva, angular){
                 var totalHeight = model.layout.qHyperCube.qSize.qcy;
                 var pageHeight = Math.floor(10000 / columns);
                 var numberOfPages = Math.ceil(totalHeight / pageHeight);
+                var dataHeaders = getHeaders(model);
 
                 if (numberOfPages === 1) {
-                    (data.qDataPages) ? deferred.resolve(data.qDataPages[0].qMatrix)
-                        : deferred.resolve(data[0].qMatrix);
+                    (data.qDataPages) ? deferred.resolve(dataHeaders.concat(data.qDataPages[0].qMatrix))
+                        : deferred.resolve(dataHeaders.concat(data[0].qMatrix));
                 } else {
                     var promises = Array.apply(null, new Array(numberOfPages)).map(function (data, index) {
                         return model.getHyperCubeData('/qHyperCubeDef', [getNextPage(pageHeight,index, columns)]);
                     }, this);
-
-                    Promise.all(promises).then(function(data){
+                    Promise.all(promises).then(function(data){                        
                         for (var j = 0; j < data.length; j++) {
                             if (data[j].qDataPages) {
                                 for (var k1 = 0; k1 < data[j].qDataPages[0].qMatrix.length; k1++) {
                                   qTotalData.push(data[j].qDataPages[0].qMatrix[k1]);
                                 }
                             } else {
-                                for (var k2 = 0; k2 < data[j][0].qMatrix.length; k2++) {                        
+                                for (var k2 = 0; k2 < data[j][0].qMatrix.length; k2++) {
                                   qTotalData.push(data[j][0].qMatrix[k2]);
                                 }
                             }
                         }
-                        deferred.resolve(qTotalData);
+                        deferred.resolve(dataHeaders.concat(qTotalData));
                     })
                 }
             })
@@ -60,5 +60,15 @@ define(['qlik', 'qvangular', 'angular'], function(qlik, qva, angular){
             qHeight: pageHeight,
             index: index
         }
+    }
+
+    function getHeaders(model) {
+        var dimensions = model.layout.qHyperCube.qDimensionInfo;
+        var measures = model.layout.qHyperCube.qMeasureInfo;
+
+        dimensions = dimensions.map(function(dim){ return { qText: dim.qFallbackTitle }; })
+        measures = measures.map(function(measure){ return { qText: measure.qFallbackTitle }; })
+
+        return [ dimensions.concat(measures) ];
     }
 })
